691f28f6059c25807520bb4419f7eca1af5eea50
high-cde <2.58428056e+08+high-cde@users.noreply.github.com>
2026-09-07 22:10:09 +0000

    fix: watch nativewind css cache in metro

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
+const path = require("path");
 
 const config = getDefaultConfig(__dirname);
+config.watchFolders = [path.resolve(__dirname, "node_modules/react-native-css-interop/.cache")];
 
 module.exports = withNativeWind(config, {
   input: "./global.css",
